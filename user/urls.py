8a0f63dea4600adef315c4139111670ce2c851f2
Bikash <kan076bct021@kec.edu.np>
2025-05-23 14:27:23 +0545

    register

--- user/urls.py
+++ user/urls.py
@@ -4,5 +4,6 @@ urlpatterns=[
     path('',homepage,name='homepage'),
     path('productpage/',productpage,name='productpage'),
     path('productdetail/<int:product_id>/', productdetail, name='productdetail'),
+    path('register/',register,name="register")
     
 ]
--- user/views.py
+++ user/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from product.models import *
 from .filters import *
 
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS, 'User account has been created successfully.')
+            return redirect('/register')
+        else:
+            messages.add_message(request, messages.ERROR, 'Please provide correct credentials.')
+            return render(request, 'user/register.html', {'form': form})
+
+    context = {
+        'form': UserCreationForm()
+    }
+    return render(request, 'user/register.html', context)
+
+
 def homepage(request):
     product = Product.objects.all().order_by('-id')[:4]  # Get last 4 products
     data = {
